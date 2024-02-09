56d8b6d826e2bdc871bd81ad71c5d59f18c0ed93
Dyke-F <dyke.ferber@t-online.de>
2024-02-10 00:48:36 +0100

    ..

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="med-self-discover",
-    version="0.1",
+    version="0.1.1",
     license="MIT",
     description="""
         Implementation of the Code from the Paper SELF-DISCOVER: Large Language Models Self-Compose Reasoning Structures,
@@ -19,9 +19,9 @@ setup(
     author="Dyke Ferber",
     author_email="dykeferber@gmail.com",
     install_requires=[
-        "openai=1.12.0",
-        "tenacity=8.2.3",
-        "fire=0.5.0",
-        "termcolor=2.4.0",
+        "openai>=1.12.0",
+        "tenacity>=8.2.3",
+        "fire>=0.5.0",
+        "termcolor>=2.4.0",
     ],
 )
